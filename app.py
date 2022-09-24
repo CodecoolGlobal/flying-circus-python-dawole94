b42f91e6e41f9a39cee7e2c5c52608879ea055a6
damskw <damian.skwierawski@gmail.com>
2022-09-24 16:37:09 +0200

    Finish

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def verify_password(plain_text_password, hashed_password):
 
 
 def is_loggedin():
+
     return 'user_email' in session
 
 @app.route('/')
@@ -36,10 +37,43 @@ def login():
     #
     password = request.form.get('password')
     if verify_password(password, data.users[email]):
-
+        session["user_email"] = email
+        session["points"] = 0
+        session["question_count"] = 0
         return redirect('/')
     return render_template('login.html', message='Wrong password!')
 
+
+@app.route('/logout')
+def logout():
+    session.pop("user_email", None)
+    session.pop("points", None)
+    session.pop("question_count", None)
+    return redirect('/login')
+
+@app.route('/test', methods=["GET", "POST"])
+def test():
+    if not is_loggedin():
+        return redirect('/login')
+    if session["question_count"] >= len(data.questions.items()):
+        return redirect('/result')
+    question = list(data.questions.items())[session["question_count"]]
+    if request.method == "GET":
+        return render_template('test.html', question=question)
+    answers = list(question[1].items())
+    answer = request.form.get("answer")
+    answer = int(answer)
+    session["question_count"] += 1
+    if answers[answer][1]:
+        session["points"] += 1
+    return redirect('/test')
+
+
+@app.route('/result')
+def result():
+    if not is_loggedin():
+        return redirect('/login')
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
